242e294e7286a46691a572ecf1e6bf21b0899c2a
Hector <hector@agilekitchen.io>
2017-09-11 11:54:26 -0500

    added Results Component

---
+++ app/components/Results.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+
+class Results extends Component {
+
+    render() {
+        return (
+            <div>
+                Results
+            </div>
+        );
+    }
+}
+
+module.exports = Results;
